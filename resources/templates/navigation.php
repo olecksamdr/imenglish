ce748810a09476ad4a1c928c0f7374ac14b923d2
Olecksndr <velsashok@gmail.com>
2017-08-16 09:10:47 +0300

    add skype and mobile phone to navbar

--- resources/templates/navigation.php
+++ resources/templates/navigation.php
@@ -36,17 +36,28 @@
 
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="nav navbar-nav">
-    <li class="active"><a href="/#courses">курсы<span class="sr-only">(current)</span></a></li>
-    <li><a href="/#prices">цены</a></li>
-    <li><a href="/reviews.php">отзывы</a></li>
+      <li class="active"><a href="/#courses">курсы<span class="sr-only">(current)</span></a></li>
+      <li><a href="/#prices">цены</a></li>
+      <li><a href="/reviews.php">отзывы</a></li>
 
-    <?php if ( isset($_SESSION['userData']) ): ?>
-      <?php if ( $_SESSION['userData']['oauth_uid'] === $config['admin']['oauth_uid'] ): ?>
-        <li><a href="/administration/manage-reviews.php">управление отзывами</a></li>
+      <?php if ( isset($_SESSION['userData']) ): ?>
+        <?php if ( $_SESSION['userData']['oauth_uid'] === $config['admin']['oauth_uid'] ): ?>
+          <li><a href="/administration/manage-reviews.php">управление отзывами</a></li>
+        <?php endif; ?>
       <?php endif; ?>
-    <?php endif; ?>
 
     </ul>
+
+    <p class="navbar-text navbar-right contacts">
+      <b class="skype">
+        <img src="img/skype-icon.png" alt="skype icon">
+        imenglish@gmail.com
+      </b>
+      <b class="phone">
+        <img src="img/smartphone.png" alt="smartphone icon">
+        +380678473060
+      </b>
+    </p>
 
     <?php if ( isset($_SESSION['userData']) ): ?>
       <button class="btn btn-default navbar-btn pull-right" id="btn-logout">выйти</dutton>
